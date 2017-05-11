edb99f4f01a87bce4797f35dfc945adafbbb4036
joshgressman <joshgressman@gmail.com>
2017-05-11 13:05:50 -0500

    added life cycle hooks demo

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked {
   @Input('srvElement') element: {type: string, name: string, content: string}; //can now be bound with another element outside of the component
-  constructor() { }
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  constructor() {
+   console.log('constructor called');
+  }
 
   ngOnInit() {
+    console.log('ngOnInit Called');
+    console.log('text content:', this.header.nativeElement.textContent);
+  }
+
+  ngOnChanges(changes: SimpleChanges){ //changs from @input() element
+    console.log('ngOnChanges Called');
+    console.log('changes', changes);
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called');
   }
 
 }
